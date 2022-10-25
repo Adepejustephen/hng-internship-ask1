747525827c059e1142e78ab13bcf382c99ba70dd
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-10-25 23:41:54 +0100

    add socials to links component

---
+++ src/assets/svgs/index.js
@@ -0,0 +1,6 @@
+import Github from './github.svg'
+import I4G from './I4G.svg'
+import Slack from './slack.svg'
+import Zuri from './zuri-logo.svg'
+
+export {Github, I4G, Slack, Zuri}
--- src/components/links/index.jsx
+++ src/components/links/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { Github, Slack } from '../../assets';
 import { links } from '../../data'
 import './links.css'
 
 export const LinksComp = () => {
   return (
-      <div className="links">
-          {links.map((item, id) => {
-              
-              return (
-                <a href={item.link}>
-                  <button className="links__btn" id={item.id} key={item.id}>
-                  
-                    {item.title}
-                  </button>
-                </a>
-              ); 
-          })}
+    <div className="links">
+      <div className="links__list">
+        {links.map((item, id) => {
+          return (
+            <a href={item.link} target="blank" id={item.id} key={item.id}>
+              <button className="links__btn">{item.title}</button>
+            </a>
+          );
+        })}
+          </div>
+          <div className="links__socials">
+              <img src={Slack} alt="slack-icon" />
+              <img src={Github} alt="github-icon" />
+          </div>
     </div>
-  )
+  );
 }
