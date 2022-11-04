ac55003e23158c7de43c924316f3cadb41401818
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-11-04 17:16:46 +0100

    add ids to contact elements

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -12,23 +12,25 @@ export const ContactUi = () => {
       <form action="" className="contact__form">
         <div className="contact__form--group">
           <div className="contact__form--item">
-            <label htmlFor="email" className="contact__form--label">
+            <label htmlFor="first_name" className="contact__form--label">
               First name
             </label>
             <input
               type="text"
               className="contact__form--input"
               placeholder="Enter your first name"
+              id="first_name"
             />
           </div>
           <div className="contact__form--item">
-            <label htmlFor="email" className="contact__form--label">
+            <label htmlFor="last_name" className="contact__form--label">
               Last name
             </label>
             <input
               type="text"
               className="contact__form--input"
               placeholder="Enter your last name"
+              id="last_name"
             />
           </div>
         </div>
@@ -40,15 +42,17 @@ export const ContactUi = () => {
             type="email"
             className="contact__form--input"
             placeholder="yourname@email.com"
+            id="email"
           />
         </div>
         <div className="contact__form--item">
-          <label htmlFor="email" className="contact__form--label">
+          <label htmlFor="message" className="contact__form--label">
             Message
           </label>
           <textarea
             className="contact__form--input"
             placeholder="Send me a message and I'll reply you as soon as possible..."
+            id="message"
           />
         </div>
         <div className="contact__form--group">
@@ -58,7 +62,9 @@ export const ContactUi = () => {
             you.
           </label>
         </div>
-        <botton className="btn">Send message</botton>
+        <botton className="btn" id="btn__submit">
+          Send message
+        </botton>
       </form>
     </div>
   );
